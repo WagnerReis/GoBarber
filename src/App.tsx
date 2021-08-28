14b81e615ad96e380cc2a0cbe066d1386f60cfe2
WagnerReis <wagnerreis1997@gmail.com>
2021-08-28 17:53:06 -0300

    Página de cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import Signin from './pages/Signin';
+import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
   return (
     <>
-      <Signin />
+      <SignUp />
       <GlobalStyle />
     </>
   );
