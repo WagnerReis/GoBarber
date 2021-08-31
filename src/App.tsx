68674c5b89e9e04fc54331ed4f60909320ef8b34
WagnerReis <wagnerreis1997@gmail.com>
2021-08-31 11:29:08 -0300

    Validação de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import Signin from './pages/Signin';
-import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+// import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
   return (
     <>
-      <SignUp />
+      <SignIn />
       <GlobalStyle />
     </>
   );
