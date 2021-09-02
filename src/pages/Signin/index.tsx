bda669b30c0013009314e53c425218e096e67e40
WagnerReis <wagnerreis1997@gmail.com>
2021-09-02 14:15:01 -0300

    Login pelo contexto

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -5,7 +5,7 @@ import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
-import AuthContext from '../../context/AuthContext';
+import { AuthContext } from '../../context/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
@@ -13,12 +13,17 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { name } = useContext(AuthContext);
+  const { signIn } = useContext(AuthContext);
 
-  const handleSubmit = useCallback(async(data: object) => {
+  const handleSubmit = useCallback(async(data: SignInFormData) => {
     try {
       formRef.current?.setErrors({});
 
@@ -32,6 +37,11 @@ const SignIn: React.FC = () => {
       await schema.validate(data, {
         abortEarly: false,
       });
+
+      signIn({
+        email: data.email,
+        password: data.password,
+      });
     }catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
@@ -40,7 +50,7 @@ const SignIn: React.FC = () => {
         return;
       }
     }
-  },[]);
+  },[signIn]);
 
   return (
   <Container>
