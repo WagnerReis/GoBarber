bc410bb19d1eecbe14cf0e6f4c68a16b2ebb58c0
WagnerReis <wagnerreis1997@gmail.com>
2021-09-14 14:25:46 -0300

    Configurando rotas + animação

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -4,6 +4,7 @@ import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+import { Link } from 'react-router-dom';
 
 import { useAuth } from '../../hooks/auth';
 import { useToast } from '../../hooks/toast';
@@ -12,7 +13,7 @@ import getValidationErrors from '../../utils/getValidationErrors';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Content, Background } from './styles';
+import { Container, Content, AnimationContainer, Background } from './styles';
 
 interface SignInFormData {
   email: string;
@@ -63,23 +64,25 @@ const SignIn: React.FC = () => {
   return (
   <Container>
     <Content>
-      <img src={logoImg} alt='GoBarber' />
+      <AnimationContainer>
+        <img src={logoImg} alt='GoBarber' />
 
-      <Form ref={formRef} onSubmit={handleSubmit}>
-        <h1>Faça seu logon</h1>
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu logon</h1>
 
-        <Input name='email' icon={FiMail} placeholder='E-mail'/>
-        <Input name='password' icon={FiLock} type="password" placeholder='Senha'/>
+          <Input name='email' icon={FiMail} placeholder='E-mail'/>
+          <Input name='password' icon={FiLock} type="password" placeholder='Senha'/>
 
-        <Button type="submit">Entrar</Button>
+          <Button type="submit">Entrar</Button>
 
-        <a href="forgot">Esqueci minha senha</a>
-      </Form>
+          <a href="forgot">Esqueci minha senha</a>
+        </Form>
 
-      <a href="login">
-        <FiLogIn />
-        Criar conta
-      </a>
+        <Link to="/signup">
+          <FiLogIn />
+          Criar conta
+        </Link>
+      </AnimationContainer>  
     </Content>
 
     <Background />
