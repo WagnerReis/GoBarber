a51a707d18dea40b162f88cd69d6bb2965beef07
WagnerReis <wagnerreis1997@gmail.com>
2021-09-14 10:51:51 -0300

    Adicionando e removendo Toasts

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -52,7 +52,11 @@ const SignIn: React.FC = () => {
         return;
       }
 
-      addToast();
+      addToast({
+        type: 'error',
+        title: 'Error na autenticação',
+        description: 'Ocorreu um erro ao fazer login, cheque as credenciais.',
+      });
     }
   },[signIn, addToast]);
 
